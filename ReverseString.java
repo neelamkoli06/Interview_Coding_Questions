6e6879e119993d1f749dce64e7cdf8f024c10444
Neelam Koli <neelamkoli06@gmail.com>
2024-04-03 07:19:41 +0530

    todays code

---
+++ ReverseString.java
@@ -0,0 +1,14 @@
+class ReverseString {
+    public static void main(String[] args) {
+
+    String str = "hello";
+    String reverse = "";
+    int length = str.length();
+
+    for(int i = 0;i<length;i++)
+    {
+        reverse = str.charAt(i) + reverse;
+    }
+    System.out.println(reverse);
+    }
+}
