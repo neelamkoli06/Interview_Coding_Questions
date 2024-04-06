f54d8ffce84e6a6933b84cbf6a4a29616fde44b5
Neelam Koli <neelamkoli06@gmail.com>
2024-04-06 10:28:57 +0530

    todays code

---
+++ matchingElements.java
@@ -0,0 +1,14 @@
+//Basically here we are finding the duplicate element in the array
+
+public class matchingElements {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,5,1,2,6,7};
+        for(int i=0; i< a.length; i++){
+            for(int j = i+1; j<a.length;j++){
+                if(a[j]==a[i]){
+                    System.out.println(a[i]);
+                }
+            }
+        }
+    }
+}
---
+++ reverseArray.java
@@ -0,0 +1,16 @@
+public class reverseArray{
+    public static void main(String args[]){
+        int[] b = new int[] {1,2,3,4,5,6,7,8};
+        System.out.println("original array: ");
+
+        for(int i=0; i<b.length;i++){
+            System.out.print(b[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Array in reverse order: ");
+
+        for(int i=b.length-1; i>=0; i--){
+            System.out.print(b[i] + " ");
+        }
+    }
+}
