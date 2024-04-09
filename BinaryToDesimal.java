8b4eb153c066b6ccf81e2d04d71c9f2271a4c14c
Neelam Koli <neelamkoli06@gmail.com>
2024-04-09 20:38:27 +0530

    todays code

---
+++ BinaryToDesimal.java
@@ -0,0 +1,13 @@
+public class BinaryToDesimal {
+    public static void main(String[] args) {
+        int N = 11011;
+        int a = 1;
+        int ans = 0;
+        while(N != 0){
+            ans = ans + (N % 10) * a;
+            N = N/10;
+            a = a * 2;
+        }
+        System.out.println(ans);
+    }
+}
---
+++ LargestNumber.java
@@ -0,0 +1,15 @@
+class LargestNumber {
+    public static void main(String[] args) {
+        int a=1, b=2, c=3;
+
+        if(a > b && a > c){
+            System.out.println(a + "means a is greater!");
+        }
+        else if(b > a && b > c){
+            System.out.println(b + " means b is greater");
+        }
+        else{
+            System.out.println(c + " c is greater");
+        }
+    }
+}
