09fe2974f71a29755c020d6c294ea9ed6c3008ca
Aarti Koli <aartikoli09@gmail.com>
2024-04-20 11:41:07 +0530

    todays code

---
+++ Star_Pattern/StarPyramid.java
@@ -0,0 +1,15 @@
+public class StarPyramid {
+    public static void main(String[] args) {
+        int n = 5, k=0;
+        for(int i = 1; i<=n; ++i,k=0){
+            for(int j = 1; j <= n-i; ++j){
+                System.out.print("  ");
+            }
+            while(k != 2 * i-1){
+                System.out.print("* ");
+                ++k;
+            }
+            System.out.println();
+        }
+    }
+}
