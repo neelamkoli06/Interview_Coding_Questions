a07572777a760de4c0f858f0759b509161826228
Neelam Koli <neelamkoli06@gmail.com>
2024-04-30 11:12:50 +0530

    todays code

---
+++ Star_Pattern/Reverse_Row_Alpha.java
@@ -0,0 +1,13 @@
+class Reverse_Row_Alpha {
+    public static void main(String[] args) {
+        int n = 5;
+        char currentChar = 'A';
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= i ;j++){
+                System.out.print(currentChar + " ");
+                currentChar++;
+            }
+            System.out.println();
+        }
+    }
+}
